221b7eeca942b0f982612b2bc720160d0c153669
KurtOtto90 <kurtotto90@gmail.com>
2022-08-30 21:46:28 -0500

    complete bar chart

--- charts.js
+++ charts.js
@@ -78,18 +78,27 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = otu_ids.slice(0,10).map(id => {id}).reverse();
+    var yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
     console.log(yticks)
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
+    var barData = [{
+      x: sample_values.slice(0,10).reverse(),
+      y: yticks,
+      text:otu_labels,
+      type: "bar",
+      orientation: 'h'
+    }
       
     ];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+     title: "Top 10 Bacteria Cultures Found",
+     xaxis: {title: "Sample Values"},
+     yaxis: {title: "IDs"}
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot("bar", barData, barLayout);
+
   });
 }
